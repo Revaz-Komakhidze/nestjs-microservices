64f5c8738792fb31d0e6042373ef45166afa6361
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2024-05-06 20:15:39 +0400

    switched from http to nats

--- apps/workflows-service/src/workflows/workflows.controller.ts
+++ apps/workflows-service/src/workflows/workflows.controller.ts
@@ -1,21 +1,14 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
-import { WorkflowsService } from './workflows.service';
 import { CreateWorkflowDto, UpdateWorkflowDto } from '@app/workflows';
+import { Body, Controller, Delete, Get, Param, Patch } from '@nestjs/common';
+import { WorkflowsService } from './workflows.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('workflows')
 export class WorkflowsController {
   constructor(private readonly workflowsService: WorkflowsService) {}
 
-  @Post()
-  create(@Body() createWorkflowDto: CreateWorkflowDto) {
+  @MessagePattern('workflows.create')
+  create(@Payload() createWorkflowDto: CreateWorkflowDto) {
     return this.workflowsService.create(createWorkflowDto);
   }
 
